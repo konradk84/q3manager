f668395e66b62d3f69c763218e2df33a38047afe
konradk84 <konrad.karasinski84@gmail.com>
2019-01-11 19:19:49 +0100

    Working. TODO: parse repsond u lazzy mf

--- q3info.py
+++ q3info.py
@@ -1,7 +1,5 @@
 import socket, select, sys, os, subprocess, threading
 
-from querys import set_info_query, set_status_query
-
 #servers = [ 'ctf.q3msk.ru', 'meat.q3msk.ru', 'q3msk.ru:7790', 'q3msk.ru', 'q3msk.ru:27961', 'q3msk.ru:27962', 'q3msk.ru:27963', 'q3msk.ru:27964' ]
 
 class run_thread(threading.Thread):
@@ -12,11 +10,34 @@ class run_thread(threading.Thread):
         result = run_quake(self.server)
 
 def run_quake(server):
-    #os.chdir('e:\g\q3')
-    #subprocess.call(['cd e:\g\q3'])
+    os.chdir('e:\g\q3')
     q = 'quake3.exe +connect ctf.q3msk.ru'
-    #subprocess.Popen(q)
-    print('working')
+    subprocess.Popen(q)
+
+def connect(server_dict):
+    while True:
+        server_count = show_servers(server_dict)
+        choice = input('\nenter server number to connect or (c) to cancel:\n ')
+        if choice == 'c':
+            break
+        else:
+            if validate_choice(choice, server_count):
+                #print('mamy: ', server_dict[1])
+                #print('mamy: ', server_dict[int(choice)])
+                server = server_dict[int(choice)]
+                #input()
+                run_thread(server).start()
+                break
+
+def set_info_query(base):
+    info = base
+    info += b'getinfo xxx'
+    return info
+
+def set_status_query(base):
+    status = base
+    status += b'getstatus xxx'
+    return status
 
 def make_servers_dic():
     if not os.path.isfile(servers_file):
@@ -45,6 +66,10 @@ def show_servers(server_dict):
         print(server, server_dict[server])
     return servers_count
 
+def count_servers(server_dict):
+    servers_count = len(server_dict)
+    return servers_count
+
 def add_server():
     get_servers = open(servers_file, 'a')
     #print('enter server address or address:port')
@@ -55,16 +80,28 @@ def add_server():
         print('server with port: ', server_from_user)
     get_servers.write(server_from_user + '\n')
     get_servers.close()
+    return show_servers(make_servers_dic())
+
+def validate_choice(choice, servers_count):
+    if choice.isdigit():
+        if int(choice) <= servers_count and int(choice) > 0:
+            return True
+        else:
+            print('wrong number\n')
+            return False
+    else:
+        print('wrong number\n')
+        return False
 
 def remove_server(server_dict):
     while True:
         servers_count = show_servers(server_dict)
         #print('\nenter server number to remove or (e) to exit')
-        choice = input('\nenter server number to remove or (e) to exit: ')
-        if choice == 'e':
+        choice = input('\nenter server number to remove or (c) to cancel:\n ')
+        if choice == 'c':
             break
-        elif choice.isdigit():
-            if int(choice) <= servers_count and int(choice) > 0:
+        else:
+            if validate_choice(choice, servers_count):
                 file = open(servers_file, 'r')
                 server_lines = file.readlines()
                 file.close()
@@ -75,56 +112,46 @@ def remove_server(server_dict):
                 file.close()
                 print('server removed')
                 return show_servers(make_servers_dic())
-            else:
-                print('wrong number\n')    
-        else:
-            print('wrong number\n')
-            continue
 
 def parse_respond(buf):
     print('buf: ', buf)
 
-def check_port(servers):
-    #for server in servers:
-    '''
-    if any(":" in s for s in server):
-        port_index = server.find(':')
-        port = server[port_index : len(server)]
-        port = port.strip(':')
-        server = server[0 : port_index]
-        return server
-    else:
-        port = 27960
-        scan_servers(server, port, set_info_query(base))
-        '''
-def scan_servers(server, port, info):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    port = int(port)
-    print('server: ', server,  'port: ', port)
-    sock.connect((server, port))
-    if (sock.sendto(info, (server, port)) == 1):
-        print('sendto error')
+def scan_servers(server_dict, info):
+    for server in range(1, count_servers(server_dict)+1):
+        print('SERVER: ' , server)
+        if any(":" in s for s in server_dict[server]):
+            port_index =server_dict[server].find(':')
+            port = server_dict[server][port_index : len(server_dict[server])]
+            port = port.strip(':')
+            server = server_dict[server][0 : port_index]
+        #TODO:
+        #else:
+            #tu jest ryzyko, ze jesli ktos doda z palca cos to pliku i nie bedzie tam : to sie zesra
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        port = int(port)
+        sock.connect((server, port))
+        if (sock.sendto(info, (server, port)) == 1):
+            print('sendto error')
+        while (True):
+            buf = ''
+            timeout = 1
+            r,w,e = select.select([sock], [], [], timeout)
+            if sock in r:
+                buf = sock.recvfrom(1024)
+                parse_respond(buf)
+                break
+            else:
+                print('no data')
+        if (sock.sendto(info, (server, port)) == 1):
+            print('sendto error')
+    os.system("PAUSE")
+    #TODO: po udanym skanowaniu zatrzymaj os.system("PAUSE")
 
-    while (True):
-        buf = ''
-        timeout = 1
-        r,w,e = select.select([sock], [], [], timeout)
-        if sock in r:
-            buf = sock.recvfrom(1024)
-            parse_respond(buf)
-            break
-        else:
-            print('no data')
-    
-    if (sock.sendto(info, (server, port)) == 1):
-        print('sendto error')
 def manage_choice(choice):
     if choice == '1':
-        check_port(servers)
-        os.system("PAUSE")
+        scan_servers(make_servers_dic(), set_info_query(base))
     elif choice == '2':
-        server = 'asd'
-        run_thread(server).start()
+        connect(make_servers_dic())        
     elif choice == '3':
         show_servers(make_servers_dic())
     elif choice == '4':
